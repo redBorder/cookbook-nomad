dd1d561e70e2687df5f97e191add6e0df989d1cb
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-06-24 14:08:21 +0200

    Add default values to client attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,13 +3,17 @@
 # Attributes:: default
 #
 
-default.nomad.user = "nomad"
-default.nomad.group = "nomad"
+default['nomad']['user'] = "nomad"
+default['nomad']['group'] = "nomad"
 
-default.nomad.data_dir = "/tmp/nomad"
-default.nomad.log_dir = "/var/log/nomad"
-default.nomad.name = "default-name"
-default.nomad.bind_addr = "127.0.0.1"
-default.nomad.num_schedulers = 1
-default.nomad.bootstrap_expect = 1
-default.nomad.servers = ["127.0.0.1"]
+default['nomad']['data_dir'] = "/tmp/nomad"
+default['nomad']['log_dir'] = "/var/log/nomad"
+default['nomad']['name'] = "default-name"
+default['nomad']['bind_addr'] = "127.0.0.1"
+default['nomad']['num_schedulers'] = 1
+default['nomad']['bootstrap_expect'] = 1
+default['nomad']['servers'] = ["127.0.0.1"]
+
+default['nomad']['cpu_mhz'] = 500
+default['nomad']['memory_mb'] = 512
+default['nomad']['disk_mb'] =5120
