1abaaea9d8f02583ae35b52d18328dba59203146
cjmateos <mateos83@gmail.com>
2016-08-26 13:00:35 +0200

    Fixed typo in client resource

--- resources/client.rb
+++ resources/client.rb
@@ -9,7 +9,7 @@ default_action :add
 attribute :user, :kind_of => String, :default => "nomad"
 attribute :group, :kind_of => String, :default => "nomad"
 attribute :log_dir, :kind_of => String, :default => "/var/log/nomad"
-attribute :data_dir, :kind_of => String, :default => "/tmp/nomad/client"
+attribute :data_dir, :kind_of => String, :default => "/tmp/nomad"
 attribute :name, :kind_of => String, :default => "default-client-name"
 attribute :bind_addr, :kind_of => String, :default => "127.0.0.1"
 attribute :servers, :kind_of => Array, :default => ["127.0.0.1"]
